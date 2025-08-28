c57627d5050f99b44bec9491445e0feae7fd1687
mirandabutler <mirandamayb@gmail.com>
2025-08-28 14:52:44 -0600

    add procfile to new location

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # BigDataApp
+
+To run the Flask application, go to the termnal and execute:  flask run. The application should be accessible     via this url: http://127.0.0.1:5000/.
+```
+cd my_flask_app/src
+flask run
+```
--- my_flask_app/src/app.py
+++ my_flask_app/src/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -17,3 +18,39 @@ def main():
 def echo_input():
     input_text = request.form.get("user_input", "")
     return "You entered: " + input_text
+
+
+
+
+
+
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Users.sqlite3'
+
+# db = SQLAlchemy(app)
+
+# class MusicVideos(db.Model):
+#     song = db.Column("Song_Name", db.string, primary_key=True)
+#     video = db.Column(db.string(255))
+
+
+# @app.route("/")
+# def main():
+#     return '''
+#      <form action="/echo_user_input" method="POST">
+#          <input name="user_input">
+#          <input type="submit" value="Submit!">
+#      </form>
+#      '''
+
+# @app.route("/echo_user_input", methods=["POST"])
+# def echo_input():
+#     input_text = request.form.get("user_input", "")
+#     # get data from api
+#     # save data to database
+#     return "You entered: " + input_text
+
+# class MusicVideos(db.Model):
+#     song = db.Column("Song_Name", db.string, primary_key=True)
+#     video = db.Column(db.string(255))
